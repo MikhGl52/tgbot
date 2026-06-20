fd5e163b5ee24a0f3d1a7b1d343757fe4c967eb4
Mikhail <mikhail@labserver.local>
2026-06-20 19:53:29 +0300

    update config.example.py

--- config.example.py
+++ config.example.py
@@ -1,11 +1,16 @@
 import os
 import platform
 
+MAX_SIZE_BYTES = 50 * 1024 * 1024  # 50 MB
+
 TOKEN = "YOUR_BOT_TOKEN_HERE"
+
+SPOTIFY_CLIENT_ID = "YOUR_SPOTIFY_CLIENT_ID"
+SPOTIFY_CLIENT_SECRET = "YOUR_SPOTIFY_CLIENT_SECRET"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if platform.system() == 'Windows':
     FFMPEG_PATH = os.path.join(BASE_DIR, 'ffmpeg', 'bin')
 else:
-    FFMPEG_PATH = None  # на Linux ffmpeg ставится через apt
+    FFMPEG_PATH = None
